0e7c00ac89dfb1344907ce6879b1478d02d7f87e
unknown <jazzja891@gmail.com>
2023-02-20 22:17:43 +0200

    commit

---
+++ src/Models/Fruits/Apple.php
@@ -0,0 +1,11 @@
+<?php
+
+// one apple object
+
+class Apple extends Fruit
+{
+    protected function setWeight() :void
+    {
+        $this->weight = mt_rand(150, 180);
+    }
+}
---
+++ src/Models/Fruits/Fruit.php
@@ -0,0 +1,29 @@
+
+<?php
+
+abstract class Fruit
+{
+    protected string $weight;
+    
+    public function __construct()
+    {
+        $this->setWeight();
+    }
+
+    /**
+     * Set weight
+     * @return void
+     */
+    abstract protected function setWeight(): void;
+
+
+    /**
+     * Get the value of weight
+     *
+     * @return string
+     */
+    public function getWeight(): string
+    {
+        return $this->weight;
+    }
+}
